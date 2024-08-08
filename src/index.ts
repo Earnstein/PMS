3b8743e073d99ccc05f16602b5b5ead38ad503ee
earnstein <bdamilola00@gmail.com>
2024-08-08 19:54:59 +0100

    feat: server entry with cluster setup to handle multiple request

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import cluster from "cluster";
+import * as os from "os";
+import { ExpressServer } from "./app";
+import "colorts/lib/string";
+
+const numCPUs = navigator?.hardwareConcurrency || os.cpus().length;
+
+function StartServer() {
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`.white.bgGreen);
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code: number, signal: string) => {
+      console.log(
+        `worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+          .red
+      );
+      setTimeout(() => cluster.fork(), 1000);
+    });
+  } else {
+    const server = new ExpressServer();
+
+    process.on("uncaughtException", (error: Error) => {
+      console.error(
+        `uncaught exception in worker ${process.pid} with error ${error.message}`
+          .red.underline
+      );
+      server.closeServer();
+      setTimeout(() => {
+        cluster.fork();
+        cluster.worker?.disconnect();
+      }, 1000);
+    });
+
+    process.on("SIGINT", () => {
+      console.log("Received SIGINT. Gracefully shutting down.");
+      server.closeServer();
+      process.exit(0);
+    });
+
+    process.on("SIGTERM", () => {
+      console.log("Received SIGTERM. Gracefully shutting down.");
+      server.closeServer();
+      process.exit(0);
+    });
+  }
+}
+
+StartServer();
