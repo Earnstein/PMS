1b39f9708134722ecce9edb64c3ba709f603b27d
earnstein <bdamilola00@gmail.com>
2024-08-09 13:37:30 +0100

    feat: inititialized project controller

---
+++ src/components/projects/projects_controller.ts
@@ -0,0 +1,23 @@
+class ProjectController {
+    public addHandler(){
+
+    }
+
+    public getAllHandler(){
+
+    }
+
+    public getDetailsHandler(){
+
+    }
+
+    public async updateHandler(){
+
+    }
+
+    public async deleteHandler(){
+        
+    }
+}
+
+export default ProjectController;
