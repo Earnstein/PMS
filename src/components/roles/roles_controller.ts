17c1ee76e33d8c1970bee8a50cd98b9c83e2344f
earnstein <bdamilola00@gmail.com>
2024-08-09 12:55:48 +0100

    feat: initialised roles module

---
+++ src/components/roles/roles_controller.ts
@@ -0,0 +1,21 @@
+class RoleController {
+    public addHandler(){
+
+    }
+
+    public getAllHandler(){
+
+    }
+
+    public getDetailsHandler(){
+
+    }
+
+    public async updateHandler(){
+
+    }
+
+    public async deleteHandler(){
+        
+    }
+}
