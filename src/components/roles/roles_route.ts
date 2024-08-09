68724c6b8fabefd594ed68260e8937cc16e41883
earnstein <bdamilola00@gmail.com>
2024-08-09 13:38:15 +0100

    feat: inititialized role route

---
+++ src/components/roles/roles_route.ts
@@ -0,0 +1,23 @@
+import { Express } from "express";
+import RoleController from "./roles_controller";
+
+class RoleRoutes {
+  private baseEndPoint = "/api/roles";
+
+  constructor(app: Express) {
+    const controller = new RoleController();
+
+    app
+      .route(this.baseEndPoint)
+      .get(controller.getAllHandler)
+      .post(controller.addHandler);
+
+    app
+      .route(this.baseEndPoint + "/:id")
+      .get(controller.getDetailsHandler)
+      .put(controller.updateHandler)
+      .delete(controller.deleteHandler);
+  }
+}
+
+export default RoleRoutes;
