1b345ee07eb686f657021f2a81be737927d676b9
earnstein <bdamilola00@gmail.com>
2024-08-10 01:16:47 +0100

    feat: added abstract controller base repository.

---
+++ src/utils/base_controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+abstract class BaseController {
+    public abstract addHandler(req:Request, res:Response): void;
+    public abstract getAllHandler(req:Request, res:Response): void;
+    public abstract getOneHandler(req:Request, res:Response): void;
+    public abstract updateHandler(req:Request, res:Response): void;
+    public abstract deleteHandler(req:Request, res:Response): void;
+}
+
+export default BaseController;
