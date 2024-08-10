eb90bab4bc996cc8893a7128384696650e2fae42
earnstein <bdamilola00@gmail.com>
2024-08-10 01:14:36 +0100

    feat: connected routes to express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import { I_ServerConfig } from "./utils/config";
 import * as config from  "../config.json";
+import Routes from "./routes/index";
 
 export class ExpressServer {
     private static server = null;
@@ -9,10 +10,18 @@ export class ExpressServer {
     constructor() {
         const port = this.server_config.port || 5000;
         const app = express();
+        app.use(express.urlencoded({extended: false}));
+        app.use(express.json());
 
         app.get("/ping", (req, res) => {
             res.send("pong");
         });
+
+        const routes = new Routes(app);
+
+        if (routes) {
+            console.log('Routes connected to server.');
+        }
 
         ExpressServer.server = app.listen(port, () => {
             console.log(`Server running on port ${port} with pid ${process.pid}`);
