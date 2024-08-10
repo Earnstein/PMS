30405a028b6c159ed419d96fcfa892766c44d41c
earnstein <bdamilola00@gmail.com>
2024-08-10 18:47:39 +0100

    chore: add addHandler/getAllHandler for role class

--- src/components/roles/roles_controller.ts
+++ src/components/roles/roles_controller.ts
@@ -1,23 +1,43 @@
+import { Request, Response } from "express";
+import { PERMISSIONS } from "../../utils/commmon";
+import RolesService from "./roles_service";
+
+export class RolesUtil {
+  /**
+   * Retrieves all possible permissions from the defined permissions object.
+   * @returns {string[]} - An array of all possible permissions.
+   */
+  public static getAllPermissions(): string[] {
+    let permissions = [];
+    for (const modules in PERMISSIONS) {
+      if (PERMISSIONS[modules]["ALL"]) {
+        let sectionValues = PERMISSIONS[modules]["ALL"].split(",");
+        permissions.push(...sectionValues);
+      }
+    }
+    return permissions;
+  }
+}
+
 class RoleController {
-    public addHandler(){
+  public async addHandler(req: Request, res: Response): Promise<void> {
+    const role = req.body;
+    const service = new RolesService();
+    const result = await service.create(role);
+    res.status(result.statusCode).json(result);
+  }
 
-    }
+  public async getAllHandler(req: Request, res: Response): Promise<void> {
+    const service = new RolesService();
+    const result = await service.findAll(req.query);
+    res.status(result.statusCode).json(result);
+  }
 
-    public getAllHandler(){
+  public getDetailsHandler() {}
 
-    }
+  public async updateHandler() {}
 
-    public getDetailsHandler(){
-
-    }
-
-    public async updateHandler(){
-
-    }
-
-    public async deleteHandler(){
-        
-    }
+  public async deleteHandler() {}
 }
 
 export default RoleController;
