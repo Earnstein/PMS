66330f3717262cdf252be18661fe388874cfbbcd
earnstein <bdamilola00@gmail.com>
2024-08-11 23:08:16 +0100

    feat: added function to add default role

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,45 @@ import cluster from "cluster";
 import * as os from "os";
 import { ExpressServer } from "./app";
 import "colorts/lib/string";
-import { PMS_DATA_SOURCE } from './utils/db_utils';
+import { PMS_DATA_SOURCE } from "./utils/db";
 import "reflect-metadata";
-
+import { DBUtil } from "./utils/db_utils";
 
 const NUM_CPUs = os.cpus().length;
+const args = process.argv.slice(2);
 
 function StartServer() {
   if (cluster.isPrimary) {
     console.log(`Primary worker with pid ${process.pid} is running`.green.underline);
 
-    for (let i = 0; i < NUM_CPUs; i++) {
-      cluster.fork();
-    }
+    if (args.length > 0 && args[0] === "--init") {
+      (async () => {
+        await PMS_DATA_SOURCE.getInstance();
+        await DBUtil.addDefaultRoles();
+        process.exit(0);
+      })();
+    } else {
+      for (let i = 0; i < NUM_CPUs; i++) {
+        cluster.fork();
+      }
 
-    cluster.on("exit", (worker, code: number, signal: string) => {
-      console.log(`worker ${worker.process.pid} died with code ${code} and signal ${signal}`.red);
-      setTimeout(() => cluster.fork(), 1000);
-    });
+      cluster.on("exit", (worker, code: number, signal: string) => {
+        console.log(
+          `worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+            .red
+        );
+        setTimeout(() => cluster.fork(), 1000);
+      });
+    }
   } else {
     const server = new ExpressServer();
     new PMS_DATA_SOURCE();
 
     process.on("uncaughtException", (error: Error) => {
-      console.error(`uncaught exception in worker ${process.pid} with error ${error.message}`.red.underline);
+      console.error(
+        `uncaught exception in worker ${process.pid} with error ${error.message}`
+          .red.underline
+      );
       server.closeServer();
       setTimeout(() => {
         cluster.fork();
