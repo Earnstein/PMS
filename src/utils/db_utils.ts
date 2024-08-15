9c403201116b7c6cf0e3d1c79f1353c147d35461
earnstein <bdamilola00@gmail.com>
2024-08-15 17:12:52 +0100

    feat: added method for adding default user.

--- src/utils/db_utils.ts
+++ src/utils/db_utils.ts
@@ -2,6 +2,12 @@ import RolesService from "../components/roles/roles_service";
 import Roles from "../components/roles/roles_model";
 import { RolesUtil } from "../components/roles/roles_controller";
 import { v4 } from "uuid";
+import UsersService from "../components/users/users_service";
+import Users from "../components/users/users_model";
+import * as config from  "../../config.json";
+import { encryptString } from "./common";
+
+
 
 export class DBUtil {
   private static superAdminRoleID: string;
@@ -37,11 +43,30 @@ export class DBUtil {
     }
   }
 
-  // public static async addDefaultUser():  Promise<boolean> {
-  //   try {
-  //     const service = await Users
-  //   } catch (error) {
-      
-  //   }
-  // }
+  public static async addDefaultUser():  Promise<boolean> {
+    try {
+      const service = new UsersService();
+      const password = await encryptString(config.default_user.password);
+      const user: Users = {
+        user_id: v4(),
+        firstname: "Super",
+        lastname: "Admin",
+        username: "SuperAdmin",
+        email: config.default_user.email,
+        password: password,
+        role: this.superAdminRoleID,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+      const result = await service.create(user);
+      console.log("Added super Admin User", result);
+      if (result.statusCode === 201) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error(`Error Adding default user ${error.message}`);
+      return false;
+    }
+  }
 }
