73c9da746409cd51c4b55c12bad15126f794b7ee
earnstein <bdamilola00@gmail.com>
2024-08-19 02:21:00 +0100

    add: not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,42 @@
-import express, { Application } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { I_ServerConfig } from "./utils/config";
-import * as config from  "../config.json";
+import * as config from "../config.json";
 import Routes from "./routes/index";
+import { StatusCodes } from "http-status-codes";
 
 export class ExpressServer {
-    private static server = null;
-    public server_config: I_ServerConfig = config;
+  private static server = null;
+  public server_config: I_ServerConfig = config;
 
-    constructor() {
-        const port = this.server_config.port || 5000;
-        const app = express();
-        app.use(express.urlencoded({extended: false}));
-        app.use(express.json());
+  constructor() {
+    const port = this.server_config.port || 5000;
+    const app = express();
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
 
-        app.get("/ping", (req, res) => {
-            res.send("pong");
+    const routes = new Routes(app);
+
+    if (routes) {
+      console.log("Routes connected to server.");
+    };
+    
+    app.use("*", (req: Request, res: Response, next: NextFunction) => {
+        res.status(StatusCodes.NOT_FOUND).json({
+          message: "Route not found",
+          statusCode: StatusCodes.NOT_FOUND,
+          status: "failed",
         });
+        next();
+      });
+    ExpressServer.server = app.listen(port, () => {
+      console.log(`Server running on port ${port} with pid ${process.pid}`);
+    });
+  }
 
-        const routes = new Routes(app);
-
-        if (routes) {
-            console.log('Routes connected to server.');
-        }
-
-        ExpressServer.server = app.listen(port, () => {
-            console.log(`Server running on port ${port} with pid ${process.pid}`);
-        });
-    }
-
-    public closeServer(): void {
-        ExpressServer.server.close(() => {
-            console.log("Server closed");
-            process.exit(0);
-        });
-    }
+  public closeServer(): void {
+    ExpressServer.server.close(() => {
+      console.log("Server closed");
+      process.exit(0);
+    });
+  }
 }
